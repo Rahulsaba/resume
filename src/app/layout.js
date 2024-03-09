781b201da6f0410bb46528391511e5c55329fdaf
Rahulsaba <rahulsaba2000@gmail.com>
2024-03-09 07:13:38 +0530

    head

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,27 +6,28 @@ import Head from 'next/head';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Rahul hello",
+  title: "Rahul Reaume",
   description: "Online Resume Generator ",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <meta name="description" content="Checkout our cool page" key="desc" />
+        <meta property="og:title" content="Social Title for Cool Page" />
+        <meta
+          property="og:description"
+          content="And a social description for our cool page"
+        />
+        <meta
+          property="og:image"
+          content="https://rahulsabaresume.netlify.app/public/vercel.svg"
+        />
+      </Head>
       <body className={'flex min-h-screen flex-col  pt-10 pr-36 pl-36'}>
         <AntdRegistry>
-          <Head>
-            <meta name="description" content="Checkout our cool page" key="desc" />
-            <meta property="og:title" content="Social Title for Cool Page" />
-            <meta
-              property="og:description"
-              content="And a social description for our cool page"
-            />
-            <meta
-              property="og:image"
-              content="https://rahulsabaresume.netlify.app/public/vercel.svg"
-            />
-          </Head>
+
           {children}
         </AntdRegistry>
       </body>
